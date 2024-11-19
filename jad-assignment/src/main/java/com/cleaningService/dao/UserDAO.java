8ce1cd88d333543602830d05b48b11ef3e672b44
THIRI03 <thiriw12.23@ichat.sp.edu.sg>
2024-11-20 02:54:54 +0800

    verify_user in userDAO

--- jad-assignment/src/main/java/com/cleaningService/dao/UserDAO.java
+++ jad-assignment/src/main/java/com/cleaningService/dao/UserDAO.java
@@ -35,35 +35,27 @@ public class UserDAO {
     }
     
     public boolean verifyUser(String email, String password) {
-        boolean isUserLogin = false;
-        String sql = "SELECT password FROM users WHERE email=?";
-
-        try (Connection connection = DBConnection.getConnection()) {
-            System.out.println("Connected to the database.");
-            PreparedStatement statement = connection.prepareStatement(sql);
-
-            statement.setString(1, email);
-            System.out.println("Executing query: " + statement);
-
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                String storedPassword = resultSet.getString("password");
-                System.out.println("Stored password hash: " + storedPassword);
-
-                if (BCrypt.checkpw(password, storedPassword)) {
-                    System.out.println("Password match!");
-                    return true;
-                } else {
-                    System.out.println("Password does not match.");
-                    return false;
-                }
-            } else {
-                System.out.println("User not found for email: " + email);
-            }
-        } catch (SQLException e) {
+    	boolean isUserLogin = false;
+    	String sql = "SELECT password FROM users WHERE email=?";
+    	
+    	try(Connection connection = DBConnection.getConnection()) {
+    		PreparedStatement statement = connection.prepareStatement(sql);
+    		
+    		statement.setString(1,  email);
+    		ResultSet resultSet = statement.executeQuery();
+    		
+    		if(resultSet.next()) {
+    			String storedPassword = resultSet.getString("password");
+    			
+    			if(BCrypt.checkpw(password, storedPassword)) {
+    				return true;
+    			}else {
+    				return false;
+    			}
+    		}
+    	}catch (SQLException e) {
             e.printStackTrace();
         }
         return isUserLogin;
     }
-
 }
