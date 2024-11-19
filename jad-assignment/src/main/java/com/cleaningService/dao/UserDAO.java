ab7044b42e08655f60563c87fce6281829a443af
THIRI03 <thiriw12.23@ichat.sp.edu.sg>
2024-11-20 01:40:26 +0800

    hashing_password

--- jad-assignment/src/main/java/com/cleaningService/dao/UserDAO.java
+++ jad-assignment/src/main/java/com/cleaningService/dao/UserDAO.java
@@ -6,19 +6,20 @@ import java.sql.SQLException;
 
 import com.cleaningService.model.User;
 import com.cleaningService.util.DBConnection;
-
+import org.mindrot.jbcrypt.*;
 public class UserDAO {
 
     public boolean registerUser(User user) {
         boolean isUserRegistered = false;
         String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
 
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
         try (Connection connection = DBConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
 
             statement.setString(1, user.getName());
             statement.setString(2, user.getEmail());
-            statement.setString(3, user.getPassword());
+            statement.setString(3, hashedPassword);
 
             int rowsInserted = statement.executeUpdate();
             if (rowsInserted > 0) {
